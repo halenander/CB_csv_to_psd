31f5af393358c4df84355c4250b1bb471f81d9d7
Haley Lenander <halenander@gmail.com>
2023-08-29 11:26:48 -0700

    adding imports, debugging still needed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-import 'ag-psd/initialize-canvas';
-import { readPsd } from 'ag-psd';
-import { writePsd } from 'ag-psd';
-import * as fs from 'fs';
+import { readPsd } from '/node_modules/ag-psd';
+import { writePsdBuffer } from '/node_modules/ag-psd';
+import * as fs from '/node_modules/fs';
+import '/node_modules/ag-psd/initialize-canvas.js';
 
-const temp_one = fs.readFileSync('/psd-files/Kahala-Q1.psd');
-const temp_two = fs.readFileSync('/psd-files/ML-WL-INSIDE.psd');
+const buff_one = readFileSync('/psd-files/Kahala-Q1.psd');
+const buff_two = readFileSync('/psd-files/ML-WL-INSIDE.psd');
+
+const temp_one = readPsd(buff_one);
+console.log(temp_one);
 
 const updateWeb = (update) => {
     let main = document.querySelector("main"); // main is an element
