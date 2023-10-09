905acaa35faeb30fc94a0ac79e55946205dde7ed
Haley Lenander <halenander@gmail.com>
2023-10-09 15:54:17 -0700

    website running, working on ag-psd useage

--- public/js/converter.js
+++ public/js/converter.js
@@ -1,9 +1,21 @@
-// var PSD = require("psd");
-// var psd1 = PSD.fromFile("psd-files/Kahala-Q1.psd");
-// var psd2 = PSD.fromFile("psd-files/ML-WL-INSIDE.psd");
+import * as fs from 'fs';
+import 'ag-psd/initialize-canvas'; // only needed for reading image data and thumbnails
+import { readPsd } from 'ag-psd';
 
-// psd1.parse();
-// psd2.parse();
+const buffer = fs.readFileSync('/Kahala-Q1.psd');
+
+// read only document structure
+const psd1 = readPsd(buffer, { skipLayerImageData: true, skipCompositeImageData: true, skipThumbnail: true });
+console.log(psd1);
+
+// read document structure and image data
+// const psd2 = readPsd(buffer);
+// console.log(psd2);
+// by defaults `canvas` field type is HTMLCanvasElement, so it needs to be cast to `any`
+// or node-canvas `Canvas` type, in order to call `toBuffer` method
+// fs.writeFileSync('layer-1.png', (psd2.children[0].canvas as any).toBuffer());
+
+
 
 const updateWeb = (update) => {
   let main = document.querySelector("main");
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // Load Node modules
-import express from "express";
+var express = require("express");
+const ejs = require("ejs");
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static("public"));
+// Set the view engine to ejs
+app.set("view engine", "ejs");
 // Port website will run on
 app.listen(8080);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get("/", function (req, res) {
+  res.render("pages/site");
+});
 
 // import express from "express";
 // import path from "path";
